3b7081ddc63a936645e476a6d52c03d37893c0de
NorakoTenc <worldhentai69@outlook.com>
2023-10-22 23:26:12 +0300

    Add project files.

---
+++ ASP.NETLab7/Controllers/FileController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using System.Text;
+
+namespace ASP.NETLab7.Controllers
+{
+    [Route("[controller]/[action]")]
+    public class FileController : Controller
+    {
+        [HttpGet]
+        public IActionResult DownloadFile()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult DownloadFile(string firstName, string lastName, string fileName)
+        {
+            var content = $"Name: {firstName}\nSurname: {lastName}";
+            var byteArray = Encoding.UTF8.GetBytes(content);
+            var stream = new MemoryStream(byteArray);
+
+            return File(stream, "text/plain", $"{fileName}.txt");
+        }
+    }
+}
---
+++ ASP.NETLab7/Program.cs
@@ -0,0 +1,13 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+app.MapGet("/", () => "Hello World!");
+app.MapControllers();
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=File}/{action=DownloadFile}/{id?}",
+    new { controller = "File", action = "DownloadFile" }
+);
+
+app.Run();
